3d7cfa435814a202cc72e477d0805f16f5ce1537
Emiliano Slaibe <emslaibe@gmail.com>
2023-02-24 19:44:35 -0500

    complete javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,77 @@
+//Business logic
+// Pizza object constructor
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
 
-// Define pizza object constructor
-class Pizza {
-  constructor(toppings, size) {
-    this.toppings = toppings;
-    this.size = size;
+// Prototype method for calculating the cost of a pizza
+Pizza.prototype.calculateCost = function () {
+  let cost = 0;
+
+  cost += this.toppings.length;
+
+  // Add cost for the size
+  if (this.size === "large") {
+    cost += 10;
+  } else if (this.size === "small") {
+    cost += 5;
   }
 
-  // Define prototype method for calculating cost of pizza
-  cost() {
-    let cost = 0;
-    if (this.size === "medium") {
-      cost += 10;
-    } else if (this.size === "large") {
-      cost += 12;
-    }
-    cost += this.toppings.length * 1;
-    return cost;
+  return cost;
+};
+
+function ShoppingCart() {
+  this.pizzas = [];
+  this.totalPrice = 0;
+}
+
+ShoppingCart.prototype.calculateTotalCost = function () {
+  let total = 0;
+  this.pizzas.forEach(function (pizza) {
+    total += pizza.calculateCost();
+  });
+
+  return total;
+};
+
+// User Interface
+// Function for getting the selected toppings and size, creating a new pizza object, and displaying the cost
+function calculateCost() {
+
+  // Collect a list of checkboxes with the name of "topping" that were checked 
+  // querySelectorAll can give an array or undefined
+  const selectedToppings = [];
+  const toppingsChecked = document.querySelectorAll('input[name="topping"]:checked');
+  if (toppingsChecked !== undefined) {
+    toppingsChecked.forEach(function (topping) {
+      // i'm pushing this checked topping into the array of sleected toppings
+      selectedToppings.push(topping);
+    });
+  }
+
+  // note from mentor:
+  // A much more professional way and less "error-prone" way to do this is by:
+  // const selectedToppings = Array.from(document.querySelectorAll('input[name="topping"]:checked'))
+  //   .map((input) => input.value);
+
+  const selectedSizeRadio = document.querySelector('input[name="size"]:checked');
+  if (!selectedSizeRadio) {
+    // error
+    document.querySelector("#pizza-form-error").innerHTML =
+      "Please select a size";
+  } else {
+    const selectedSize = selectedSizeRadio.value;
+    const pizza = new Pizza(selectedToppings, selectedSize);
+    const cost = pizza.calculateCost();
+    document.getElementById("cost").textContent = `Total cost: $${cost}`;
   }
 }
 
-// Add event listener for form submission
-document.querySelector("form").addEventListener("submit", (event) => {
-  event.preventDefault();
-  const toppings = [];
-  const toppingInputs = document.querySelectorAll('input[name="topping"]:checked');
-  toppingInputs.forEach((input) => toppings.push(input.value));
-  const size = document.querySelector('input[name="size"]:checked').value;
-  const pizza = new Pizza(toppings, size);
-  const totalCost = pizza.cost();
-  document.getElementById("total-cost").textContent = totalCost;
+const pizzaForm = document.querySelector("#pizza-form");
+
+pizzaForm.addEventListener("submit", () => {
+  console.log("Submitted form");
+  document.querySelector("#pizza-form-error").innerHTML = "";
+  calculateCost();
 });
